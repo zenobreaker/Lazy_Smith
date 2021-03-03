a5576bcf4120dfbf13e157cc5c8eea899daed2a7
turtlezeno <wpsh6369@gmail.com>
2021-03-03 14:07:32 +0900

    테스트1

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("게임 매니저 테스트");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
