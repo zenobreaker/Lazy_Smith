77f3f367d755be7cf72e058b1524a3591b83569c
turtlezeno <wpsh6369@gmail.com>
2021-03-06 04:56:08 +0900

    TimeGauageController
    해당 클래스 내용 추가 및 UI 추가

---
+++ Assets/02.Scripts/TimeGauageController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeGauageController : MonoBehaviour
+{
+    [SerializeField] Slider timeSlider = null;
+    [SerializeField] Image img_fillGauage = null;
+
+    public bool isStart; 
+
+    private void LateUpdate()
+    {
+        if(isStart)
+        {
+            timeSlider.value -= Time.deltaTime;
+            ChangeFillColor();
+        }
+    }
+
+    // 슬라이더 최대값 변경 
+    public void SetMaxValue(float p_Max)
+    {
+        timeSlider.maxValue = p_Max;
+    }
+
+    void ChangeFillColor()
+    {
+        float currentValue = timeSlider.value / timeSlider.maxValue * 100;
+
+        if (currentValue > 60)
+        {
+            img_fillGauage.color = Color.green;
+        }else if(currentValue <= 60 && currentValue > 30)
+        {
+            img_fillGauage.color = Color.yellow;
+        }else
+            img_fillGauage.color = Color.red;
+    }
+}
