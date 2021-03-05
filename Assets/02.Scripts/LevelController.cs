508f8be887c3f015c08e4202484df13bf1dfc7af
turtlezeno <wpsh6369@gmail.com>
2021-03-06 04:19:33 +0900

    LevelController 생성

---
+++ Assets/02.Scripts/LevelController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    // 레벨 증가 
+    void IncreaseLevel()
+    {
+
+    }
+
+    // 레벨에 따른 노트 수 계산 
+    public int CalcNoteCount(int p_Level) {
+        
+        return 0;
+    }
+}
